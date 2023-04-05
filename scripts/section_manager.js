5c15c9dcb2cfd954b61363889aefe198794b6761
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2023-04-06 00:18:38 +0530

    Added timeout logic to increase performance and efficiency. Changed function structure to class structure for section_manager.js

--- scripts/section_manager.js
+++ scripts/section_manager.js
@@ -1,14 +1,17 @@
 	
-	'use strict';
+'use strict';
 
-	function SectionManager(sectionClass) {
+class SectionManager {
+	constructor(sectionClass) {
+		this.timeoutVal = 1;
 		this.sections = $(sectionClass);
 		this.sectionIndex = 0;
 		this.sectionIdData = 'section-id';
+		this.visitedLast = false;
 
 		//fill array with 'top' position of each section
 		this.sectionTopArr = [];
-		$.each(this.sections, (i,e) => {
+		$.each(this.sections, (i, e) => {
 			this.sectionTopArr[i] = Math.floor($(this.sections[i]).offset().top) - 60;
 		});
 		this.sectionTopArr[0] += 60;
@@ -25,42 +28,43 @@
 				this.scrollY = e.currentTarget.scrollY;
 				this.scrollY = (this.scrollY < 0) ? 0 : this.scrollY;
 				this.updateSectionIndex();
-				if(this.callback != undefined)
+				if (this.callback != undefined)
 					this.callback(scrollY);
-			}, 1);
+			}, this.timeoutVal);
 		});
-	};
-
-	SectionManager.prototype.init = function() {
+	}
+	init() {
 		this.scrollY = $(window).scrollTop();
 		this.scrollY = (this.scrollY < 0) ? 0 : this.scrollY;
 		this.updateSectionIndex();
 		this.onSectionIndexChange();
-	};
-
-	SectionManager.prototype.updateSectionIndex = function() {
+	}
+	updateSectionIndex() {
 		var flag = true;
 		var temp;
-		for(var i = 0; i < this.sectionTopArr.length - 1; i++) {
-			if(this.scrollY >= this.sectionTopArr[i] && this.scrollY < this.sectionTopArr[i + 1]) {
+		for (var i = 0; i < this.sectionTopArr.length - 1; i++) {
+			if (this.scrollY >= this.sectionTopArr[i] && this.scrollY < this.sectionTopArr[i + 1]) {
 				temp = i;
 				flag = false;
 				break;
 			}
 		}
-		if(flag) temp = this.sectionTopArr.length - 1;
-		if(temp != this.sectionIndex) {
+		if (flag) {
+			temp = this.sectionTopArr.length - 1;
+			this.visitedLast = true;
+		}
+		if (temp != this.sectionIndex) {
 			this.sectionIndex = temp;
 			//callback
 			this.onSectionIndexChange();
+			console.log(this.timeoutVal);
 		}
-	};
-
-	SectionManager.prototype.onSectionIndexChange = function() {
+	}
+	onSectionIndexChange() {
 		//Event callback listener for Section Index changed
 		//update navbar position
-		if(this.navbar != undefined || this.navbar != null) {
-			if(this.scrollY >= this.navbarScrollY) {
+		if (this.navbar != undefined && this.navbar != null) {
+			if (this.scrollY >= this.navbarScrollY) {
 				this.navbar.addClass('position-fixed');
 				this.navbarPlaceholder.height(this.navbarHeight);
 			}
@@ -70,31 +74,37 @@
 			}
 		}
 		//update the current nav to reflect the current section
-		if(this.navItems != undefined || this.navItems != null) {
+		if (this.navItems != undefined && this.navItems != null) {
 			this.navItems.removeClass('m-color-accent');
-			$.each(this.navItems, (i,e) => {
-				if($(e).data(this.sectionIdData) == this.sectionIndex) {
+			$.each(this.navItems, (i, e) => {
+				if ($(e).data(this.sectionIdData) == this.sectionIndex) {
 					$(e).addClass('m-color-accent');
 					return false;
 				}
 			});
 		}
-	};
-
-	SectionManager.prototype.nextSection = function() {
+		//update timeout val to increase efficiency and decrease load
+		if (this.sectionIndex > 1 && this.visitedLast) {
+			this.timeoutVal = 50;
+		}
+		else {
+			this.timeoutVal = 1;
+		}
+	}
+	nextSection() {
 		this.sectionIndex++;
-		if(this.sectionIndex >= this.sections.length) this.sectionIndex = 0;
+		if (this.sectionIndex >= this.sections.length)
+			this.sectionIndex = 0;
 		this.scrollTo('#' + $(this.sections[this.sectionIndex]).attr('id'), true, 600);
 		this.scrollY = $(this.sections[this.sectionIndex]).offset().top;
-	};
-
-	SectionManager.prototype.goToSection = function(sectionIndex) {
-		if(sectionIndex >= this.sections.length || sectionIndex < 0) return;
+	}
+	goToSection(sectionIndex) {
+		if (sectionIndex >= this.sections.length || sectionIndex < 0)
+			return;
 		this.sectionIndex = sectionIndex - 1;
 		this.nextSection();
-	};
-
-	SectionManager.prototype.setNavbar = function(navbar, navitems) {
+	}
+	setNavbar(navbar, navitems) {
 		this.navbar = $(navbar);
 		this.navItems = $(navitems);
 		this.navbarPlaceholder = $('.navbar-placeholder');
@@ -103,21 +113,29 @@
 		//setup nav click events
 		this.navItems.click((e) => {
 			var t = $(e.currentTarget).data(this.sectionIdData);
-			if(t != this.sectionIndex) this.goToSection(t);
+			if (t != this.sectionIndex)
+				this.goToSection(t);
 		});
-	};
-
-	SectionManager.prototype.scrollTo = function(elementSelector, enableAnim, animTime) {
-		if(enableAnim) {
+	}
+	scrollTo(elementSelector, enableAnim, animTime) {
+		if (enableAnim) {
 			$('html, body').animate({
-	        	scrollTop: $(elementSelector).offset().top
-	    	}, animTime);
+				scrollTop: $(elementSelector).offset().top
+			}, animTime);
 		}
 		else {
 			$('html, body').scrollTop($(elementSelector).offset().top);
 		}
-	};
-
-	SectionManager.prototype.setOnControlledScrollCallBack = function(callback) {
+	}
+	setOnControlledScrollCallBack(callback) {
 		this.callback = callback;
-	};
+	}
+};
+
+
+
+
+
+
+
+
